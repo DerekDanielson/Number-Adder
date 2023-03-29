eff9d0cfe77008a22363995a2ccf78a31838d342
Derek <derekdanielson84@gmail.com>
2023-03-29 10:12:19 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const submit = document.getElementById('submit');
+const output = document.getElementById('#output');
+
+submit.addEventListener('click', addNum);
+
+function addNum(num1, num2){
+    let sum = num1 + num2;
+    console.log(sum);    
+}
+
+
+
+
+
