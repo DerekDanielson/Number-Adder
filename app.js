3536479ec3eda29dd53b0f805fe6ae194aff9fc3
Derek <derekdanielson84@gmail.com>
2023-03-30 09:03:49 -0600

    JS cleanup

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const submit = document.getElementById('submit');
 const output = document.getElementById('output');
 
 
+// Listen for event
 submit.addEventListener('click', function(){
-    
+    // Get input field values
     let number1 = Number(document.getElementById('num1').value);
     let number2 = Number(document.getElementById('num2').value);
-    
+    // Add numbers to sum
     let sum = number1 + number2;
     output.innerHTML = sum;    
 });
