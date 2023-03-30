eb35dfa867177a677f23ebd4c4cddc95ad12ac3c
Derek <derekdanielson84@gmail.com>
2023-03-30 08:36:10 -0600

    Result display added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const num1 = document.getElementById('num1');
 const num2 = document.getElementById('num2');
 const submit = document.getElementById('submit');
-const output = document.getElementById('#output');
+const output = document.getElementById('output');
 
 
 submit.addEventListener('click', function(){
@@ -10,7 +10,7 @@ submit.addEventListener('click', function(){
     let number2 = Number(document.getElementById('num2').value);
     
     let sum = number1 + number2;
-    alert(sum);    
+    output.innerHTML = sum;    
 });
 
 
